6c6b2e8db2149ded20cfda05493c63aa45b56395
semyhist <semih.aydin142@gmail.com>
2025-08-24 13:57:57 +0300

    ─░lk y├╝kleme

--- src/App.js
+++ src/App.js
@@ -9,25 +9,25 @@ import { Sun, Moon, Instagram, X, Calendar, User, Users, MapPin, Star, Wind, Loa
 // =================================================================================================
 const team = {
   management: [
-    { name: 'Aydın', bio: 'Genel sorumlu, kurucu ve yönetici.', emoji: '⚡' },
+    { name: 'Aydın', bio: 'Genel sorumlu, kurucu ve yönetici.', emoji: '👑' },
     { name: 'Melih', bio: 'Sorumlu, ekip içi yönetici.', emoji: '🧠' },
-    { name: 'Yankı', bio: 'Yeni, genel sorumlu ve içerik sorumlusu.', emoji: '🎨' },
+    { name: 'Yankı', bio: 'Yeni, genel sorumlu ve içerik sorumlusu.', emoji: '🧭' },
   ],
   admin: [
-    { name: 'Nisa', bio: 'İçerik ve Ekip içi denge sorumlusu', emoji: '🏎️' },
-    { name: 'Mehmet Selim', bio: '', emoji: '🏎️' },
-    { name: 'Ela', bio: '@oscarpiyastri', emoji: '🏎️' },
+    { name: 'Nisa', bio: 'İçerik ve Ekip içi denge sorumlusu', emoji: '🕷️' },
+    { name: 'Mehmet Selim', bio: '', emoji: '🌹' },
+    { name: 'Ela', bio: '@oscarpiyastri', emoji: '🧊' },
     { name: 'Azra', bio: '@ferrarisefiri', emoji: '🏎️' },
     { name: 'Yunus Emre', bio: '@orek.edits', emoji: '🏎️' },
     { name: 'Yağız', bio: '@vettelsexuel', emoji: '🏎️' },
   ],
   Twitter: [
-    { name: 'Utku', bio: 'Haber Departmanı yetkilisi.', emoji: '🏎️' },
-    { name: 'Furkan', bio: 'Haber Departmanı yetkilisi.', emoji: '🏎️' },
-    { name: 'Gencer', bio: '', emoji: '🏎️' },
-    { name: 'Taha Yasin', bio: '', emoji: '🏎️' },
-    { name: 'Utku', bio: '', emoji: '🏎️' },
-    { name: 'Melisa', bio: '', emoji: '🏎️' },
+    { name: 'Utku', bio: 'Haber Departmanı yetkilisi.', emoji: '💻' },
+    { name: 'M. Furkan', bio: 'Haber Departmanı yetkilisi.', emoji: '🐙' },
+    { name: 'Gencer', bio: '', emoji: '☯️' },
+    { name: 'Taha Yasin', bio: '', emoji: '🐊' },
+    { name: 'Melisa', bio: '', emoji: '🎀' },
+    { name: 'Furkan', bio: '', emoji: '🏎️' },
   ]
 };
 
